f51a5045c8189e3dca5dc39f82c41df3b3634af8
sonjoysaha36 <sonjoysaha36@gmail.com>
2022-09-16 20:48:31 +0600

    Using Web Forms API

--- API Check Validation/script.js
+++ API Check Validation/script.js
@@ -1,6 +1,17 @@
 function validation(){
     const inputObj = document.getElementById('id1');
 
+
+    if(inputObj.validity.rangeOverflow){
+        inputObj.setCustomValidity("You have made a range overflow error!");
+    }
+    else if(inputObj.validity.rangeUnderflow){
+        inputObj.setCustomValidity("You have made a range underflow error!");
+    }
+    else if(inputObj.validity.valueMissing){
+        inputObj.setCustomValidity("Value missing!");
+    }
+
     if(!inputObj.checkValidity()){
         document.getElementById("demo").innerHTML = inputObj.validationMessage;
     }
