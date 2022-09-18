e6d673080ae5f872829d44421d42b3942435a065
sonjoysaha36 <sonjoysaha36@gmail.com>
2022-09-18 18:29:37 +0600

    Using Worker API

---
+++ API worker/script.js
@@ -0,0 +1,25 @@
+let w;
+
+function startWorker(){
+    if(typeof Worker !== "undefined"){
+        if(typeof w == "undefined"){
+            w = new Worker ("startWorker.js");
+        }
+        w.onmessage = function(event) {
+            document.getElementById("demo").innerHTML = event.data;
+        };
+    }else{
+        alert("Your browser does not support web worker!");
+    }
+}
+
+function stopWorker(){
+    if(typeof Worker !== "undefined"){
+
+    
+    w.terminate();
+    w = undefined;
+    }else{
+        alert("Your browser does not support web worker!");
+    }
+}
